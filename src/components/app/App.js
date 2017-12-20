54d1b595dd6788846ccd340b99a459069e62636b
Nick Bond <bond.n@husky.neu.edu>
2017-12-20 16:10:06 -0500

    initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import TitleSection from '../title-section/title-section';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <TitleSection/>
+      </div>
+    );
+  }
+}
+
+export default App;
