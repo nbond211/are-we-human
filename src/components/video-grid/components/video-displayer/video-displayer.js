ff8722579f5e0d6d10677fc68d01852ce875c739
Nick Bond <bond.n@husky.neu.edu>
2017-12-21 14:38:14 -0500

    fix title and play

--- src/components/video-grid/components/video-displayer/video-displayer.js
+++ src/components/video-grid/components/video-displayer/video-displayer.js
@@ -3,19 +3,59 @@ import ReactPlayer from 'react-player'
 import './video-displayer.css';
 
 class VideoDisplyer extends Component {
+    constructor(props) {
+        super();
+        this.props = props;
+        this.state = { isPlaying: false, mouseOver: false };
+    }
+
+    mouseOver = () => {
+        this.setState({mouseOver: true});
+    };
+
+    mouseOut = () => {
+        this.setState({mouseOver: false});
+    };
+
+    playVideo = () => {
+        this.setState({isPlaying: true});
+    };
+
+    stopVideo = () => {
+        this.setState({isPlaying: false});
+    };
+
     render() {
-        const { video, otherTitle, isHovering } = this.props;
+        const { video } = this.props;
 
         return (
-            <div class="video-displayer">
-                {otherTitle &&
-                    <div style={{display: isHovering ? 'flex' : 'none'}} class="video-title-container">
-                        <h4 class="video-title animated fadeInUp">{otherTitle}</h4>
+            <div
+                class="video-displayer col-xs-6"
+                onMouseOver={() => this.mouseOver()}
+                onMouseOut={() => this.mouseOut()}
+            >
+                {video &&
+                    <div style={{display: this.state.mouseOver && !this.state.isPlaying ? 'flex' : 'none'}} class="video-title-container">
+                        <h4 class="video-title animated fadeInUp">
+                            {video.title}
+                            <br/>
+                            <span onClick={() => this.playVideo()} class="play-video">Play Video</span>
+                        </h4>
+
                     </div>
                 }
                 {video &&
                     <div>
-                        <ReactPlayer width={480} height={360} url={video.url}/>
+                        <ReactPlayer
+                            playing={this.state.isPlaying}
+                            width={480}
+                            height={360}
+                            url={video.url}
+                            onStart={() => this.playVideo()}
+                            onPlay={() => this.playVideo()}
+                            onPause={() => this.stopVideo()}
+                            onEnded={() => this.stopVideo()}
+                        />
                     </div>
                 }
 
--- src/components/video-grid/video-grid.js
+++ src/components/video-grid/video-grid.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './video-grid.css';
-import VideoRow from './components/video-row/video-row';
+import VideoDisplayer from './components/video-displayer/video-displayer';
 import videos from '../../data/videos';
 require('smoothscroll-polyfill').polyfill();
 
@@ -8,31 +8,28 @@ class VideoGrid extends Component {
     constructor(props) {
         super();
         this.props = props;
-        this.state = {rowIndex: 2};
+        this.state = {index: 4};
     }
 
     loadMore = () => {
-      const curRowIndex = this.state.rowIndex;
-      this.setState({rowIndex: curRowIndex + 2});
+      const curIndex = this.state.index;
+      this.setState({index: curIndex + 4});
       setTimeout(() => window.scrollBy({ top: 600, left: 0, behavior: 'smooth' }), 1000);
     };
 
     render() {
-        let videoRows = [];
-        for (let i = 1; i <= videos.length; i += 2) {
-            const video1 = videos[i - 1];
-            const video2 = videos[i];
-            videoRows.push((
-                <VideoRow video1={video1} video2={video2}/>
-            ));
-        }
+        const videoDisplayers = videos.map(video => {
+           return (
+               <VideoDisplayer video={video}/>
+           )
+        });
 
         return (
             <div class="row">
                 <div class="col-lg-offset-1 col-lg-10">
-                    {videoRows.slice(0, this.state.rowIndex)}
                     <div class="row">
-                        {videoRows.length > this.state.rowIndex &&
+                        {videoDisplayers.slice(0, this.state.index)}
+                        {videoDisplayers.length > this.state.index &&
                             <div class="load-more-button-container col-xs-12">
                                 <button class="load-more-button" onClick={() => this.loadMore()}>Load More</button>
                             </div>
